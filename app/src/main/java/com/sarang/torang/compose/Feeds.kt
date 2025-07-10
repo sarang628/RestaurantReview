3e60893d20809e84eb2f9ad9cdecc52154c4937c
sryang <sarang628@gmail.com>
2025-07-10 15:27:14 +0800

    Initial commit

---
+++ app/src/main/java/com/sarang/torang/compose/Feeds.kt
@@ -0,0 +1,15 @@
+package com.sarang.torang.compose
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
+
+typealias Feeds = @Composable (Int, Modifier) -> Unit
+
+val LocalFeeds = compositionLocalOf<Feeds> {
+    // 기본 구현: 경고 로그 출력
+    @Composable { _, _ ->
+        Text("피드 모듈이 등록되지 않았습니다.")
+    }
+}
---
+++ app/src/main/java/com/sarang/torang/data/Feed.kt
@@ -0,0 +1,51 @@
+package com.sarang.torang.data
+
+data class Feed(
+    val reviewId: Int,
+    val restaurantId: Int,
+    val userId: Int,
+    val name: String,
+    val restaurantName: String,
+    val rating: Float,
+    val profilePictureUrl: String,
+    val likeAmount: Int,
+    val commentAmount: Int,
+    val author: String,
+    val author1: String,
+    val author2: String,
+    val comment: String,
+    val comment1: String,
+    val comment2: String,
+    val isLike: Boolean,
+    val isFavorite: Boolean,
+    val visibleLike: Boolean,
+    val visibleComment: Boolean,
+    val contents: String,
+    val reviewImages: List<String> = ArrayList()
+) {
+    companion object {
+        fun empty() = Feed(
+            reviewId = 0,
+            restaurantId = 0,
+            userId = 0,
+            name = "",
+            restaurantName = "",
+            rating = 0.0f,
+            profilePictureUrl = "",
+            likeAmount = 0,
+            commentAmount = 0,
+            author = "author",
+            author1 = "author1",
+            author2 = "author2",
+            comment = "comment",
+            comment1 = "comment1",
+            comment2 = "comment2",
+            isLike = true,
+            isFavorite = true,
+            visibleLike = true,
+            visibleComment = true,
+            contents = "contents",
+            reviewImages = listOf()
+        )
+    }
+}
